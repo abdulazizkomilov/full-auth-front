24a071bab8b1c7ce2a53baa3bbac689c1f482e3f
Abdulaziz <abdulazizkomilov2001@gmail.com>
2023-12-25 10:16:43 +0500

    deployment

---
+++ components/forms/index.ts
@@ -0,0 +1,4 @@
+export { default as Form } from './Form';
+export { default as Input } from './Input';
+export { default as LoginForm } from './LoginForm';
+export { default as RegisterForm } from './RegisterForm';
